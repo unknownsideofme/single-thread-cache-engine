2288426805d8ad80b82c97931289876d8e47852e
unknownsideofme <rijudebanjane303@gmail.com>
2026-01-06 18:36:12 +0530

    LRU Cache

--- include/cache.h
+++ include/cache.h
@@ -1,23 +1,32 @@
 #pragma once
-#include <iostream>
 #include <unordered_map>
 #include <string> 
 #include <mutex>
 #include "json.hpp"
 #include <chrono>
 #include <optional>
+#include <list>
 using json = nlohmann::json;
 class Cache {
     private: 
         std::unordered_map<std::string, json> cache_map;
         std::mutex mtx;
         std::unordered_map<std::string, std::chrono::steady_clock::time_point> expiration_map;
+        std::list<std::string> lru_list;
+        size_t max_size = 1000;
+        std::unordered_map<std::string, std::list<std::string>::iterator> lru_map;
+
         bool isExpired(const std::string &key);
+        void touchKey(const std::string &key);
+        void evictIfNeeded();
+
     public:
-        Cache() = default ; 
-        ~Cache() = default ;        
+        Cache(size_t cap = 100  ): max_size(cap) {} ; 
+        ~Cache() = default ;  
+        
+        
         void set(const std::string &key , const json &value );
-        std::optional<json> get( const std::string key) ; 
+        std::optional<json> get( const std::string &key) ; 
         void del(const std::string &key) ;
         void ttlExpire() ; 
 
--- src/cache.cpp
+++ src/cache.cpp
@@ -22,40 +22,68 @@ void Cache:: set(std::string const &key , json const &value){
     std::lock_guard<std::mutex> lock(mtx) ; 
     cache_map[key] = value ;
     expiration_map[key] = std::chrono::steady_clock::now() + std::chrono::minutes(5) ;
-    for(auto &it: cache_map){
-        std::cout << "Key: " << it.first << " Value: " << it.second.dump() << std::endl;
-    }
+    touchKey(key) ;
+    evictIfNeeded() ;
 
 }
 
-std::optional<json> Cache::get ( std::string key){
+std::optional<json> Cache::get ( const std::string &key){
     std::lock_guard<std::mutex> lock(mtx) ;     
     auto it = cache_map.find(key) ; 
-    if(it != cache_map.end()){
-        if(isExpired(key)){
-            cache_map.erase(it);
-            expiration_map.erase(key);
-            return std::nullopt;
-        }
-        return it->second ; 
-    }
-    return std::nullopt ;
+    if(it == cache_map.end()) return std::nullopt ;
+    if ( isExpired(key)) return std::nullopt ;
+    touchKey(key) ;
+    return it->second ;
+    
+
 }
 
 void Cache:: del ( std::string const & key){
     std::lock_guard<std::mutex> lock(mtx) ; 
     cache_map.erase(key) ; 
     expiration_map.erase(key) ; 
+    if(lru_map.find(key) != lru_map.end()){
+        lru_list.erase(lru_map[key]) ; 
+        lru_map.erase(key) ; 
+    }
 }
 
-void Cache::ttlExpire(){
+void Cache::ttlExpire() {
     std::lock_guard<std::mutex> lock(mtx);
+
     for (auto it = expiration_map.begin(); it != expiration_map.end(); ) {
         if (isExpired(it->first)) {
-            cache_map.erase(it->first);
+            const std::string key = it->first;
+            cache_map.erase(key);
             it = expiration_map.erase(it);
+            auto lru_it = lru_map.find(key);
+            if (lru_it != lru_map.end()) {
+                lru_list.erase(lru_it->second);
+                lru_map.erase(lru_it);
+            }
+
         } else {
             ++it;
         }
+    }
+}
+
+
+void Cache::touchKey( const std::string &key){
+    if(lru_map.find(key) != lru_map.end()){
+        lru_list.erase(lru_map[key]) ; 
+    }
+
+    lru_list.push_front(key) ;
+    lru_map[key] = lru_list.begin() ;
+}
+
+void Cache::evictIfNeeded(){
+    while(cache_map.size() > max_size){
+        std::string lru_key = lru_list.back() ;
+        lru_list.pop_back() ; 
+        lru_map.erase(lru_key) ; 
+        cache_map.erase(lru_key) ; 
+        expiration_map.erase(lru_key) ; 
     }
 }
